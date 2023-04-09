d84a75cc65d96ee83438b73d1b7d278a50ac93ac
jo3kr <joelmjohn16@gmail.com>
2023-04-08 19:04:02 -0500

    forgot to git add the userRoutes whihc have now been added, wrote htmlRoutes.js, created middleware to check for authentication, created login and signup logic

---
+++ config/middleware/isAuthenticated.js
@@ -0,0 +1,10 @@
+const isAuthenticated = (req, res, next) => {
+    if (req.session.logged_in) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  };
+  
+  module.exports = isAuthenticated;
+  
---
+++ controllers/html/htmlRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const isAuthenticated = require('../config/middleware/isAuthenticated');
+
+// renders the homepage with posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// renders the login/signup page
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
